e03077ff62da407af03a337f56a0cc4404d8218c
Fredrik Norgren <fredrik_norgren@hotmail.com>
2025-06-09 09:30:32 +0200

    Add project files.

---
+++ Slutprojekt MVC Grupp 4.Web/Program.cs
@@ -0,0 +1,15 @@
+namespace Slutprojekt_MVC_Grupp_4.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
