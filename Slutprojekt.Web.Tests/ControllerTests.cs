4daec9676feca6b782221dd91fadbf70636b51f1
Astrid Grytberg <25738809+agrvsk@users.noreply.github.com>
2025-06-10 17:15:08 +0200

    Modellvalidering i ControllerTests

--- Slutprojekt.Web.Tests/ControllerTests.cs
+++ Slutprojekt.Web.Tests/ControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Moq;
 using Slutprojekt.Application.Breeds.Interfaces;
@@ -6,6 +7,7 @@ using Slutprojekt.Application.Breeds.Services;
 using Slutprojekt.Domain.Entities;
 using Slutprojekt.Web.Controllers;
 using Slutprojekt.Web.Views.Breeds;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Slutprojekt.Web.Tests
 {
@@ -28,35 +30,63 @@ namespace Slutprojekt.Web.Tests
         [Theory]
         //   [InlineData(new InsertBreedVM { Id = 1, BreedType = 1, BreedName = "Schäfer", Description = "tysk hund" }, true)]
         [InlineData(3, 1, "Schäfer", "tysk hund", true)]
-        [InlineData(4, 99, "", "lejonlik", false)]
+        [InlineData(4, 99, "Tax", "", false)]
+        [InlineData(5, 2, null, "", false)]
+        [InlineData(6, null, "Sankt Bernard", "", false)]
+
         public async Task InsertBreedWithParamsExpectCorrectViewModelAsync(int id, int breedType, string breedName, string description, bool expected)
         {
             //Arrange
             var viewModel = new InsertBreedVM { Id = id, BreedType = breedType, BreedName = breedName, Description = description };
-            //var breedController = GetBreedsController();
+
             var o = GetBreedsController();
             var breedController = o.Item1;
             var breedServiceMock = o.Item2;
             var breedTypeServiceMock = o.Item3;
 
+            //Testa Validering för modelbinding
+            var context = new ValidationContext(viewModel);
+            var results = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(viewModel, context, results, validateAllProperties: true);
+            
+            if (!isValid)
+            {
+                foreach (var item in results)
+                {
+                    switch (viewModel.Id) 
+                    {
+                        case 6:
+                    //        Assert.Equal("Breedtype is required", item.ErrorMessage);
+                    //        break;
+                        case 4: Assert.Equal("Breedtype 1-10", item.ErrorMessage);
+                            break;
+                        case 5: Assert.Equal("Breedname is required", item.ErrorMessage);
+                            break;
+                        default: break;
+                    }
+                    breedController.ModelState.AddModelError("TEST",item.ErrorMessage);
+                }
+            }
 
             // Act
-            if (!expected)
-            {
-                breedController.ModelState.AddModelError("BreedName", "BreedName är obligatoriskt");
-            }
+            //if (!expected)
+            //{
+            //    breedController.ModelState.AddModelError("BreedName", "BreedName är obligatoriskt");
+            //}
 
             var result = await breedController.InsertBreed(viewModel);
 
             // Assert
             if (expected)
             {
+                Assert.True(isValid);
                 var redirect = Assert.IsType<RedirectToActionResult>(result);
                 Assert.Equal(nameof(breedController.Index), redirect.ActionName);
                 breedServiceMock.Verify(s => s.AddBreedAsync(It.IsAny<Breed>()), Times.Once);
             }
             else
             {
+                Assert.False(isValid);
                 var viewResult = Assert.IsType<ViewResult>(result);
                 Assert.IsType<InsertBreedVM>(viewResult.Model);
                 breedServiceMock.Verify(s => s.AddBreedAsync(It.IsAny<Breed>()), Times.Never);
