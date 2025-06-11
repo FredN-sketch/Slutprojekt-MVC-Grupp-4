58554ad572a30bf2f1b0d644f032cf2600364425
KakanDrakan <tobias.lindskog@yahoo.com>
2025-06-11 10:37:59 +0200

    Enhance login form with placeholders and display names
    Updated `Login.cshtml` to add placeholder text for the
    "Username" and "Password" input fields, improving user
    experience. Removed label elements for a cleaner look.
    
    In `LoginVM.cs`, added `Display` attributes for both
    properties to provide user-friendly names in the UI.

--- Slutprojekt MVC Grupp 4.Web/Views/Account/LoginVM.cs
+++ Slutprojekt MVC Grupp 4.Web/Views/Account/LoginVM.cs
@@ -5,10 +5,12 @@ namespace Slutprojekt.Web.Views.Account
     public class LoginVM
     {
         [Required]
+        [Display(Name = "Username")]
         public string Username { get; set; } = null!;
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; } = null!;
     }
 }
